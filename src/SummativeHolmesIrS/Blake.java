57bba2f8f0d56aaec3f35958e535edff141df582
Garrett <Garrett@192.168.0.31>
2019-05-20 23:23:13 -0400

    added stats and stat generation/fetch/change methods

--- src/SummativeHolmesIrS/Blake.java
+++ src/SummativeHolmesIrS/Blake.java
@@ -1,10 +1,77 @@
 package SummativeHolmesIrS;
+
 import java.util.Random;
 
 public class Blake {
+	private int endurance;
+	private int perception;
+	private int strength;
+	private int charisma;
+	private int agility;
+	private int addiction;
 
+	// change if adding more stat numbers
+	int statAmt = 6;
+	
+	int[] stats = new int[statAmt];
+	
 	public Blake() {
-		// TODO Auto-generated constructor stub
+		for (int i = 0; i < statAmt; i++) {
+			Random rand = new Random();
+			stats[i] = rand.nextInt(20);
+		}
+		endurance = stats[0];
+		perception = stats[1];
+		strength = stats[2];
+		charisma = stats[3];
+		agility = stats[4];
+		addiction = stats[5];
+	}
+	
+	public Blake(int[] stats) {
+		endurance = stats[0];
+		perception = stats[1];
+		strength = stats[2];
+		charisma = stats[3];
+		agility = stats[4];
+		addiction = stats[5];
+	}
+
+	public int[] getStats() {
+		return stats;
+	}
+	
+	public int getStat(int i) {
+		try {
+			return stats[i];
+		}catch(IndexOutOfBoundsException e){
+			System.out.println("Invalid Stat input");
+		}
+		return -1;
+	}
+
+	public void incEndurance(int amt) {
+		endurance += amt;
+	}
+
+	public void incPerception(int amt) {
+		perception += amt;
+	}
+		
+	public void incStrength(int amt) {
+		strength += amt;
+	}
+
+	public void incCharisma(int amt) {
+		charisma += amt;
+	}
+
+	public void incAgility(int amt) {
+		agility += amt;
+	}
+
+	public void incAddiction(int amt) {
+		addiction += amt;
 	}
 
 }
